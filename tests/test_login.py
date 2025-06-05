856c20932d7ab448d1441b48b125805aadb5b15a
Aswath9800 <aswath.s@publicissapient.com>
2025-06-05 11:45:14 +0530

    Pipeline

--- tests/test_login.py
+++ tests/test_login.py
@@ -4,20 +4,25 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import pytest
 
+
 @pytest.fixture
 def driver():
     driver = webdriver.Chrome()
     yield driver
     driver.quit()
 
+
 def test_successful_login(driver):
     driver.get("https://the-internet.herokuapp.com/login")
     driver.find_element(By.ID, "username").send_keys("tomsmith")
     driver.find_element(By.ID, "password").send_keys("SuperSecretPassword!")
     driver.find_element(By.CSS_SELECTOR, "button[type='submit']").click()
-    success_msg = driver.find_element(By.CSS_SELECTOR, ".flash.success").text
-    assert "You logged into a secure area!" in success_msg
 
+    # Use WebDriverWait to wait for the success message to be present
+    success_msg = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.CSS_SELECTOR, ".flash.success"))
+    ).text
+    assert "You logged into a secure area!" in success_msg
 def test_invalid_login(driver):
     driver.get("https://the-internet.herokuapp.com/login")
     driver.find_element(By.ID, "username").send_keys("wronguser")
